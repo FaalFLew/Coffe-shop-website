287ca5a36becd6fb4842c3874b56bc23456badd1
Faalflew <majdsabbas@gmail.com>
2023-05-13 01:16:30 +0200

    user registration information is stored in database, organized code for register and login component

--- Project/frontend/src/pages/Login.js
+++ Project/frontend/src/pages/Login.js
@@ -1,32 +1,67 @@
-import '../css/Global.css'
-import '../css/Login.css'
-import {Link} from 'react-router-dom'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../css/Global.css';
+import '../css/Login.css';
 
 const Login = () => {
+  const [inputs, setInputs] = useState({});
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setInputs((prevInputs) => ({ ...prevInputs, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
+
   return (
     <main className="login-container">
- <form>
- 
-  <div className="container">
-    <h1>Login</h1>
-    <p>Please fill in this form to create an account.</p>
-    <hr />
-    <label htmlFor="email"><b> Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required />
-    
-     <label htmlFor="password"><b> Password</b></label>
-    <input type="password" placeholder="Password" name="password" required />
-    <hr />
-    <button type="submit" className="register-button">Create Account</button>
-  </div>
-  
-  <div className="container signin">
-    <p>Dont have an account? <Link className="input-link"  to="/register">Register Account</Link>.</p>
-  </div>
+        <div className="container">
+        <form onSubmit={handleSubmit}>
 
- </form>
-</main>
-  )
-}
+          <h1>Login</h1>
+          <p>Please fill in this form to create an account.</p>
+          <hr />
+            <label htmlFor="email">
+              <b>Email</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Email"
+              name="email"
+              onChange={handleInput}
+              required
+            />
+            <label htmlFor="password">
+              <b>Password</b>
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleInput}
+              required
+            />
+          <hr />
+          <button type="submit" className="register-button">
+            Login
+          </button>
+          </form>
 
-export default Login
+        </div>
+      <div className="container signin">
+        <p>
+          Don't have an account?{' '}
+          <Link className="input-link" to="/register">
+            Register Account
+          </Link>
+          .
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
--- Project/frontend/src/pages/Register.js
+++ Project/frontend/src/pages/Register.js
@@ -1,46 +1,126 @@
 import '../css/Global.css';
 import '../css/Register.css';
-import {Link} from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from 'axios'
 
 const Register = () => {
+  const [inputs, setInput] = useState({ Newsletter_subscribed: false });
+
+  const handleInput = (e) => {
+    const { name, value, checked, type } = e.target;
+    const inputValue = type === 'checkbox' ? checked : value;
+    setInput((values) => ({ ...values, [name]: inputValue }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://group15.web-tek.ninja/backend/api/login.php', inputs);
+    console.log(inputs);
+  };
+
   return (
-<main className="register-container">
- <form>
- 
-  <div className="container">
-    <h1>Create Account</h1>
-    <p>Please fill in this form to create an account.</p>
-    <hr />
-<div className="input-container" >
-    <label htmlFor="first-name"><b>First name</b></label>
-    <input type="text" placeholder="Enter First name" name="first-name" required />
-</div>
-<div className="input-container" >
-    <label htmlFor="last-name"><b>Last Name</b></label>
-    <input type="text" placeholder="Enter Last name" name="last-name" required />
-</div>
-    <label htmlFor="email"><b> Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required />
-    
-     <label htmlFor="password"><b> Password</b></label>
-    <input type="password" placeholder="Password" name="password" required />
-    
-     <label htmlFor="password-repeat"><b> Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="password-repeat"  required />
-    <hr />
-    <p>By creating an account you agree to our <a className="input-link" href="#">Terms & Privacy</a>.</p>
+    <main className="register-container">
+      <form onSubmit={handleSubmit}>
+        <div className="container">
+          <h1>Create Account</h1>
+          <p>Please fill in this form to create an account.</p>
+          <hr />
+          <div className="input-container">
+            <label htmlFor="first_name">
+              <b>First name</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter First name"
+              name="First_name"
+              onChange={handleInput}
+              required
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="Last_name">
+              <b>Last Name</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Last name"
+              name="Last_name"
+              onChange={handleInput}
+              required
+            />
+          </div>
+          <label htmlFor="Email">
+            <b> Email</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Email"
+            name="Email"
+            onChange={handleInput}
+            required
+          />
 
-    <button type="submit" className="register-button">Create Account</button>
-  </div>
-  
-  <div className="container signin">
-    <p>Already have an account? <Link className="input-link"  to="/login">Login</Link>.</p>
-  </div>
+          <label htmlFor="Password">
+            <b> Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="Password"
+            onChange={handleInput}
+            required
+          />
 
- </form>
-</main>
-  )
-}
+          <label htmlFor="password_repeat">
+            <b> Repeat Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Repeat Password"
+            name="password_repeat"
+            required
+          />
+          <input
+            type="checkbox"
+            name="Newsletter_subscribed"
+            onChange={handleInput}
+            checked={inputs.Newsletter_subscribed || false}
+          />
+          <label htmlFor="Newsletter_subscribed"> Subscribe to our Newsletter! </label>
+          <hr />
+          <p>
+            By creating an account you agree to our{' '}
+            <a className="input-link" href="#">
+              Terms & Privacy
+            </a>
+            .
+          </p>
 
-export default Register
+          <button type="submit" className="register-button">
+            Create Account
+          </button>
+          
+        </div>
+        </form>
+
+        <div className="container signin">
+          <p>
+            Already have an account?{' '}
+            <Link className="input-link" to="/login">
+              Login
+            </Link>
+            .
+          </p>
+        </div>
+    </main>
+);
+
+};
+
+export default Register;
+
+
+
+
+
